7bc8ec8c4ccd7445675eb906b59fd05ba7cd9ecb
Daniel Swingle <swingledaniel@gmail.com>
2020-11-21 21:22:46 -0700

    Added a function to run a grid search with xgboost with an ipyparallel backend; added scree plot, PCA, and t-SNE visualizations.

---
+++ motif/analysis.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn import decomposition
+from sklearn.manifold import TSNE
+
+
+def load_features(filename, scale=True):
+    df = pd.read_csv(filename, index_col=0)
+    X = preprocessing.scale(df.drop(["track_id", "genre_code"], axis=1))
+    y = df["genre_code"].to_numpy()
+
+    # normalize the data
+    if scale:
+        X = preprocessing.scale(X)
+
+    return X, y
+
+
+def plot_by_label(dim1, dim2, y):
+    color_name = {
+        0: "blue", 1: "orange", 2: "green", 3: "red", 4: "purple",
+        5: "brown", 6: "pink", 7: "gray"
+    }
+    labels = [
+        "Hip-Hop",
+        "Pop",
+        "Folk",
+        "Experimental",
+        "Rock",
+        "International",
+        "Electronic",
+        "Instrumental",
+    ]
+    colors = ["tab:" + color_name[val] for val in y]
+
+    plt.scatter(dim1, dim2, c=colors, alpha=1)
+    for i, name in color_name.items():
+        plt.scatter(
+            [], [], c=("tab:" + name),
+            marker=".", alpha=.3, label=labels[i])
+    plt.xlabel("First Component")
+    plt.ylabel("Second Component")
+    plt.legend()
+    plt.show()
+
+
+def scree_plot(filename="../data/features_v2.csv"):
+    X, y = load_features(filename)
+
+    # compute the variance explained by each principal component
+    pca = decomposition.PCA()
+    pca.fit_transform(X)
+
+    s = np.arange(1, X.shape[1] + 1)
+    variance = pca.explained_variance_ratio_
+    total_variance = sum(variance)
+
+    plt.plot(s, [sum(variance[:i]) / total_variance for i in s], label="Cumulative")
+    plt.plot(s, variance, label="Individual")
+    plt.xlabel("Number of Components")
+    plt.ylabel("Explained Variance")
+    plt.suptitle("Variance Explained by Principal Components")
+    plt.legend()
+    plt.show()
+
+
+def two_variable_pca(filename="../data/features_v2.csv"):
+    X, y = load_features(filename)
+
+    # compute the first two principal components
+    pc = decomposition.PCA(n_components=2).fit_transform(X)
+
+    plot_by_label(pc[:, 0], pc[:, 1], y)
+
+
+def t_sne(filename="../data/features_v2.csv"):
+    X, y = load_features(filename)
+
+    # embed in two dimensions
+    X_embedded = TSNE(n_components=2).fit_transform(X)
+
+    plot_by_label(X_embedded[:, 0], X_embedded[:, 1], y)
--- motif/models.py
+++ motif/models.py
@@ -1,3 +1,4 @@
+import ipyparallel as ipp
 from joblib import Parallel, delayed
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
@@ -58,7 +59,10 @@ def plot_confusion_matrix(
 
 
 def logistic_regression(
-        filename="../data/features_v2.csv", plot_matrix=False, test_size=0.3, normalize=False
+        filename="../data/features_v2.csv",
+        plot_matrix=False,
+        test_size=0.3,
+        normalize=False,
 ):
     df = pd.read_csv(filename, index_col=0)
     x = preprocessing.scale(df.drop(["track_id", "genre_code"], axis=1))
@@ -67,14 +71,20 @@ def logistic_regression(
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
     clf = LogisticRegression().fit(x_train, y_train)
     predictions = clf.predict(x_test)
-    print("Accuracy:", (len(y_test) - np.count_nonzero(predictions - y_test)) / len(y_test))
+    print(
+        "Logistic Regression Accuracy:",
+        (len(y_test) - np.count_nonzero(predictions - y_test)) / len(y_test),
+    )
 
     if plot_matrix:
         plot_confusion_matrix(y_test, predictions, genres, normalize=normalize)
 
     return clf
 
-def xgboost(filename='../data/features_v2.csv', test_size=.3, plot_matrix=False, normalize=True):
+
+def xgboost(
+        filename="../data/features_v2.csv", test_size=0.3, plot_matrix=False, normalize=True
+):
     df = pd.read_csv(filename, index_col=0)
     x = preprocessing.scale(df.drop(["track_id", "genre_code"], axis=1))
     y = df["genre_code"]
@@ -83,82 +93,101 @@ def xgboost(filename='../data/features_v2.csv', test_size=.3, plot_matrix=False,
     dtrain = xgb.DMatrix(x_train, label=y_train)
     dtest = xgb.DMatrix(x_test)
 
-    params = {'alpha': 0,
-        'eta': 0.3,
-        'gamma': 0,
-        'lambda': 1.5,
-        'max_depth': 6,
-        'num_class': 10,
-        'objective': 'multi:softmax'
+    params = {
+        "alpha": 1.3,
+        "eta": 0.8,
+        "gamma": 2.0,
+        "lambda": 10.0,
+        "num_class": 8,
+        "objective": "multi:softmax",
     }
 
     # train model with best parameters from grid search
     bst = xgb.train(params, dtrain)
 
-    dtest = xgb.DMatrix(x_test)
     predictions = bst.predict(dtest)
-    print("XGBoost Accuracy:", (len(y_test) - np.count_nonzero(predictions - y_test)) / len(y_test))
+    print(
+        "XGBoost Accuracy:",
+        (len(y_test) - np.count_nonzero(predictions - y_test)) / len(y_test),
+    )
 
     if plot_matrix:
         plot_confusion_matrix(y_test, predictions, genres, normalize=normalize)
 
     return bst
 
-def random_forest(filename='../data/features_v2.csv', test_size=.3, plot_matrix=False, normalize=True):
+
+def random_forest(
+        filename="../data/features_v2.csv", test_size=0.3, plot_matrix=False, normalize=True,
+        print_feature_importance=False
+):
     df = pd.read_csv(filename, index_col=0)
     x = preprocessing.scale(df.drop(["track_id", "genre_code"], axis=1))
     y = df["genre_code"]
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
-    
-    params = {
-        'max_depth': 10,
-        'max_features': 4,
-        'n_estimators': 150
-    }
+
+    params = {"max_depth": 10, "max_features": 4, "n_estimators": 150}
 
     clf = RandomForestClassifier()
     clf.set_params(**params)
     clf.fit(x_train, y_train)
 
-    # get feature importance
-    features = df.drop(["track_id", "genre_code"], axis=1).columns
-    imp = clf.feature_importances_
-    sorted = np.argsort(imp)
+    if print_feature_importance:
+        # get feature importance
+        features = df.drop(["track_id", "genre_code"], axis=1).columns
+        imp = clf.feature_importances_
+        sorted_features = np.argsort(imp)
 
-    print("Most-Important:", [features[i] for i in sorted[-3:]])
-    print("Least-Important:", [features[i] for i in sorted[:3]])
+        print("Most-Important:", [features[i] for i in sorted_features[-3:]])
+        print("Least-Important:", [features[i] for i in sorted_features[:3]])
 
     predictions = clf.predict(x_test)
-    print("RF Accuracy:", (len(y_test) - np.count_nonzero(predictions - y_test)) / len(y_test))
+    print(
+        "RF Accuracy:",
+        (len(y_test) - np.count_nonzero(predictions - y_test)) / len(y_test),
+    )
 
     if plot_matrix:
         plot_confusion_matrix(y_test, predictions, genres, normalize=normalize)
 
     return clf
 
-def mlp(filename='../data/features_v2.csv', test_size=.3):
+
+def mlp(filename="../data/features_v2.csv", test_size=0.3):
     df = pd.read_csv(filename, index_col=0)
     x = preprocessing.scale(df.drop(["track_id", "genre_code"], axis=1))
     y = df["genre_code"]
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
-    clf = MLPClassifier(max_iter=500, warm_start=False, hidden_layer_sizes=700, alpha=.002).fit(x_train, y_train)
+    clf = MLPClassifier(
+        max_iter=500, warm_start=False, hidden_layer_sizes=700, alpha=0.002
+    ).fit(x_train, y_train)
     predictions = clf.predict(x_test)
-    print("MLP Accuracy:", (len(y_test) - np.count_nonzero(predictions - y_test)) / len(y_test))
+    print(
+        "MLP Accuracy:",
+        (len(y_test) - np.count_nonzero(predictions - y_test)) / len(y_test),
+    )
 
-def knn(filename='../data/features_v2.csv', test_size=.3):
+
+def knn(filename="../data/features_v2.csv", test_size=0.3):
     df = pd.read_csv(filename, index_col=0)
     x = preprocessing.scale(df.drop(["track_id", "genre_code"], axis=1))
     y = df["genre_code"]
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
-    clf = KNeighborsClassifier(weights='distance', n_neighbors=100, leaf_size=50, algorithm='kd_tree')
+    clf = KNeighborsClassifier(
+        weights="distance", n_neighbors=100, leaf_size=50, algorithm="kd_tree"
+    )
     clf.fit(x_train, y_train)
     predictions = clf.predict(x_test)
-    print("KNN Accuracy:", (len(y_test) - np.count_nonzero(predictions - y_test)) / len(y_test))
+    print(
+        "KNN Accuracy:",
+        (len(y_test) - np.count_nonzero(predictions - y_test)) / len(y_test),
+    )
 
-def tune_hyperparameters(filename="../data/features_v2.csv", n_jobs=8, test_size=.3):
+
+def tune_hyperparameters(filename="../data/features_v2.csv", n_jobs=8, test_size=0.3):
     # load features
     df = pd.read_csv(filename, index_col=0)
     x = preprocessing.scale(df.drop(["track_id", "genre_code"], axis=1))
@@ -166,15 +195,17 @@ def tune_hyperparameters(filename="../data/features_v2.csv", n_jobs=8, test_size
 
     # setup parameter grid
     param_grid = {
-        "C": np.arange(.05, 1.2, .05),
-        "l1_ratio": np.arange(.005, .15, .005)
+        "C": np.arange(0.05, 1.2, 0.05),
+        "l1_ratio": np.arange(0.005, 0.15, 0.005),
     }
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
 
     # determine the score of one set of parameters
     def evaluate_params(g):
-        clf = LogisticRegression(penalty="elasticnet", multi_class="multinomial", solver="saga")
+        clf = LogisticRegression(
+            penalty="elasticnet", multi_class="multinomial", solver="saga"
+        )
         clf.set_params(**g)
         clf.fit(x_train, y_train)
         test_score = clf.score(x_test, y_test)
@@ -193,3 +224,51 @@ def tune_hyperparameters(filename="../data/features_v2.csv", n_jobs=8, test_size
             best_grid = g
 
     return best_grid, best_score
+
+
+def xgboost_grid_search():
+    """
+    Run a grid search using an xgboost classifier and return the best set of parameters and its score
+    """
+    def evaluate_params(params):
+        df = pd.read_csv("~/features.csv", index_col=0)
+        x = preprocessing.scale(df.drop(["track_id", "genre_code"], axis=1))
+        y = df["genre_code"]
+
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.3, random_state=42)
+        dtrain = xgb.DMatrix(x_train, label=y_train)
+        dtest = xgb.DMatrix(x_test)
+        n = len(y_test)
+
+        booster = xgb.train(params, dtrain)
+        predictions = booster.predict(dtest)
+        accuracy = 1.0 - np.count_nonzero(predictions - y_test) / n
+        return accuracy
+
+    # setup parameter grid for grid search
+    param_grid = list(ParameterGrid({
+        "alpha": [0, .2, .5, .7, 1, 1.3, 2, 5, 10],  # default=0
+        "eta": np.arange(0, 1.01, .1),  # default=0.3
+        "gamma": [0, .5, .8, 1, 1.3, 2, 5, 20, 100, 500],  # default=0
+        "lambda": [0, .2, .5, .7, 1, 1.3, 2, 5, 10],  # default=1
+        "num_class": [8],
+        "objective": ["multi:softmax"],
+    }))
+
+    # setup ipyparallel backend
+    rc = ipp.Client()
+    print(rc.ids)
+    print(f"{len(rc.ids)} engines connected")
+    print(f"{len(param_grid)} sets of parameters to test")
+    lview = rc.load_balanced_view()
+
+    # import necessary libraries on each engine
+    lview.execute("import pandas as pd")
+    lview.execute("from sklearn import preprocessing")
+    lview.execute("import xgboost as xgb")
+
+    # run a grid search
+    scores = lview.map(evaluate_params, tqdm(param_grid))
+    i = int(np.argmax(scores))
+
+    return param_grid[i], scores[i]
